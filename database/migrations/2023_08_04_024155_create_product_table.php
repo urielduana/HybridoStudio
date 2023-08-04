4dc97d8b383e2c3987d31cc6fbb8aa46fab404be
urielduana <uri250602@outlook.es>
2023-08-03 21:15:48 -0600

    Migrations & Models
    Migrations and seeders created for product and categories tables

---
+++ database/migrations/2023_08_04_024155_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price');
+            $table->string('barcode')->unique();
+            $table->integer('stock');
+            $table->foreignId('category_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
